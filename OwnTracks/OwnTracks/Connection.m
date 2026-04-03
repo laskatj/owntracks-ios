1fc0a8bf391b3eddba5301e9265ed246d5bb9dec
laskatj <laskatj@live.com>
2026-04-02 18:43:56 -0700

    Bugfix for MQTT Subscription keeping SLC Background running blocking futher updates.

--- OwnTracks/OwnTracks/Connection.m
+++ OwnTracks/OwnTracks/Connection.m
@@ -808,7 +808,7 @@ willPerformHTTPRedirection:(NSHTTPURLResponse *)redirectResponse
 
 #define LEN2PRINT 2048
     NSString *dataString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    DDLogInfo(@"[Connection] received topic=%@ dataString(%lu)=%@",
+    DDLogVerbose(@"[Connection] received topic=%@ dataString(%lu)=%@",
               topic,
               (unsigned long)dataString.length,
               dataString.length <= LEN2PRINT ?
